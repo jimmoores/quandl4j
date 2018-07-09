481e3040c61402801ebded8b14fe1b47a7c8639e
Ben McCann <ben@benmccann.com>
2018-07-09 10:15:02 -0700

    Upgrade Tablesaw

--- tablesaw/src/test/java/com/jimmoores/quandl/tablesaw/example/Example8.java
+++ tablesaw/src/test/java/com/jimmoores/quandl/tablesaw/example/Example8.java
@@ -1,14 +1,16 @@
 package com.jimmoores.quandl.tablesaw.example;
 
+import static tech.tablesaw.aggregate.AggregateFunctions.*;
+
 import com.jimmoores.quandl.DataSetRequest;
 import com.jimmoores.quandl.tablesaw.TableSawQuandlSession;
 
-import tech.tablesaw.api.ShortColumn;
+import tech.tablesaw.api.NumberColumn;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.api.plot.Bar;
 
 /**
- * Example 7
+ * Example 8
  */
 public class Example8 {
   /**
@@ -25,18 +27,18 @@ public class Example8 {
     Table table = session.getDataSet(
         DataSetRequest.Builder.of("WIKI/AAPL").build());
     // Create a new column containing the year
-    ShortColumn yearColumn = table.dateColumn("Date").year();
+    NumberColumn yearColumn = table.dateColumn("Date").year();
     yearColumn.setName("Year");
-    table.addColumn(yearColumn);
+    table.addColumns(yearColumn);
     // Create max, min and total volume tables aggregated by year
-    Table summaryMax = table.max("Close").by("year");
-    Table summaryMin = table.min("Close").by("year");
-    Table summaryVolume = table.sum("Volume").by("year");
+    Table summaryMax = table.summarize("Close", max).by("year");
+    Table summaryMin = table.summarize("Close", min).by("year");
+    Table summaryVolume = table.summarize("Volume", sum).by("year");
     // Create a new table from each of these
     Table summary = Table.create("Summary", summaryMax.column(0), summaryMax.column(1), summaryMin.column(1), summaryVolume.column(1));
     // Show the max close price as a graph.
     try {
-      Bar.show("Max Close Price by year", summary.shortColumn("Year"), summary.numericColumn(1));
+      Bar.show("Max Close Price by year", summary.numberColumn("Year"), summary.numberColumn(1));
     } catch (Exception e) {
       e.printStackTrace();
     }
