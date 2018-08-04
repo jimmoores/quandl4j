5232c73316e2e45eb4515f2354604d6ca210d044
Ben McCann <ben@benmccann.com>
2018-08-04 15:19:42 -0700

    Upgrade tablesaw

--- tablesaw/src/test/java/com/jimmoores/quandl/tablesaw/example/Example8.java
+++ tablesaw/src/test/java/com/jimmoores/quandl/tablesaw/example/Example8.java
@@ -1,13 +1,17 @@
 package com.jimmoores.quandl.tablesaw.example;
 
-import static tech.tablesaw.aggregate.AggregateFunctions.*;
+import static tech.tablesaw.aggregate.AggregateFunctions.max;
+import static tech.tablesaw.aggregate.AggregateFunctions.min;
+import static tech.tablesaw.aggregate.AggregateFunctions.sum;
 
 import com.jimmoores.quandl.DataSetRequest;
 import com.jimmoores.quandl.tablesaw.TableSawQuandlSession;
 
 import tech.tablesaw.api.NumberColumn;
 import tech.tablesaw.api.Table;
-import tech.tablesaw.api.plot.Bar;
+import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.components.Figure;
+import tech.tablesaw.plotly.traces.BarTrace;
 
 /**
  * Example 8
@@ -38,7 +42,7 @@ public class Example8 {
     Table summary = Table.create("Summary", summaryMax.column(0), summaryMax.column(1), summaryMin.column(1), summaryVolume.column(1));
     // Show the max close price as a graph.
     try {
-      Bar.show("Max Close Price by year", summary.numberColumn("Year"), summary.numberColumn(1));
+      Plot.show(new Figure(BarTrace.builder(summary.numberColumn("Year"), summary.numberColumn(1)).build()));
     } catch (Exception e) {
       e.printStackTrace();
     }
