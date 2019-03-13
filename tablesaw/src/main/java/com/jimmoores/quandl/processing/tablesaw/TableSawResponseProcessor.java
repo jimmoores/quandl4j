1e40ecea042b18bad59bd6146d87ff36a7ad3905
Ben McCann <322311+benmccann@users.noreply.github.com>
2019-03-12 21:25:12 -0700

    Upgrade Tablesaw

--- tablesaw/src/main/java/com/jimmoores/quandl/processing/tablesaw/TableSawResponseProcessor.java
+++ tablesaw/src/main/java/com/jimmoores/quandl/processing/tablesaw/TableSawResponseProcessor.java
@@ -9,20 +9,19 @@ import com.jimmoores.quandl.processing.TitleRequestProcessor;
 import com.jimmoores.quandl.util.QuandlRuntimeException;
 
 import tech.tablesaw.api.Table;
+import tech.tablesaw.io.csv.CsvReadOptions;
 
 public class TableSawResponseProcessor implements ResponseProcessor<Table> {
-  private static final String DEFAULT_NAME = "";
+
   private static final TitleRequestProcessor TITLE_REQUEST_PROCESSOR = new TitleRequestProcessor();
   
   public Table process(InputStream inputStream, Request request) {
     try {
-      String name;
+      String name = null;
       if (request != null) {
         name = request.accept(TITLE_REQUEST_PROCESSOR);
-      } else {
-        name = DEFAULT_NAME;
       }
-      return Table.read().csv(inputStream, name);
+      return Table.read().usingOptions(CsvReadOptions.builder(inputStream).tableName(name));
     } catch (IOException ioe) {
       throw new QuandlRuntimeException("Error reading input stream", ioe);
     }
