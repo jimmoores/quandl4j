0e1615028127730f81c744822c54fdd5b153dbe5
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-01-19 18:52:46 -0800

    Use try resource close construct

--- core/src/main/java/com/jimmoores/quandl/processing/AbstractRESTDataProvider.java
+++ core/src/main/java/com/jimmoores/quandl/processing/AbstractRESTDataProvider.java
@@ -2,6 +2,7 @@ package com.jimmoores.quandl.processing;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
@@ -49,45 +50,37 @@ public abstract class AbstractRESTDataProvider<RAW_METADATA_TYPE, TABLE_TYPE>
    */
   protected <T> T getResponse(final WebTarget target, final ResponseProcessor<T> responseProcessor, final Request request) {
     Builder requestBuilder = target.request();
-    Response response = requestBuilder.buildGet().invoke();
-    if (response.getStatus() == Response.Status.OK.getStatusCode()) {
-      InputStream inputStream = response.readEntity(InputStream.class);
-      // should we be buffering this?
-      try {
-        T result = responseProcessor.process(inputStream, request);
-        response.close();
-        inputStream.close();
-        return result;
-      } catch (IOException ex) {
-        response.close();
-        throw new QuandlRuntimeException("Problem closing input stream");
-      } catch (RuntimeException t) {
-        response.close();
+
+    try (Response response = requestBuilder.buildGet().invoke()) {
+      if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+        InputStream inputStream = response.readEntity(InputStream.class);
+        // should we be buffering this?
         try {
-          inputStream.close();
-        } catch (IOException ioe) {
+          T result = responseProcessor.process(inputStream, request);
+          return result;
+        } finally {
+          try {
+            inputStream.close();
+          } catch (IOException ioe) {
+            throw new UncheckedIOException("Problem closing input stream", ioe);
+          }
         }
-        throw t;
-      }
-    } else if (response.getStatus() == UNPROCESSABLE_ENTITY) {
-      String msg = "Response code to " + target.getUri() + " was " + response.getStatusInfo();
-      response.close();
-      throw new QuandlUnprocessableEntityException(msg);
-    } else if (response.getStatus() == TOO_MANY_REQUESTS) {
-      Long retryAfter = parseOptionalHeader(response, RETRY_AFTER);
-      Long rateLimitLimit = parseOptionalHeader(response, X_RATELIMIT_LIMIT);
-      Long rateLimitRemaining = parseOptionalHeader(response, X_RATELIMIT_REMAINING);
-      String msg = "Response code to " + target.getUri() + " was " + response.getStatusInfo();
-      response.close();
-      throw new QuandlTooManyRequestsException(msg, retryAfter, rateLimitLimit, rateLimitRemaining);
-    } else if (response.getStatus() == SERVICE_UNAVAILABLE) {
-      String msg = "Response code to " + target.getUri() + " was 503 (Service Unavailable)";
-      response.close();
-      throw new QuandlServiceUnavailableException(msg);
-    } else {
-      String msg = "Response code to " + target.getUri() + " was " + response.getStatusInfo();
-      response.close();
-      throw new QuandlRuntimeException(msg);
+      } else if (response.getStatus() == UNPROCESSABLE_ENTITY) {
+        String msg = "Response code to " + target.getUri() + " was " + response.getStatusInfo();
+        throw new QuandlUnprocessableEntityException(msg);
+      } else if (response.getStatus() == TOO_MANY_REQUESTS) {
+        Long retryAfter = parseOptionalHeader(response, RETRY_AFTER);
+        Long rateLimitLimit = parseOptionalHeader(response, X_RATELIMIT_LIMIT);
+        Long rateLimitRemaining = parseOptionalHeader(response, X_RATELIMIT_REMAINING);
+        String msg = "Response code to " + target.getUri() + " was " + response.getStatusInfo();
+        throw new QuandlTooManyRequestsException(msg, retryAfter, rateLimitLimit, rateLimitRemaining);
+      } else if (response.getStatus() == SERVICE_UNAVAILABLE) {
+        String msg = "Response code to " + target.getUri() + " was 503 (Service Unavailable)";
+        throw new QuandlServiceUnavailableException(msg);
+      } else {
+        String msg = "Response code to " + target.getUri() + " was " + response.getStatusInfo();
+        throw new QuandlRuntimeException(msg);
+      }      
     }
   }
 
